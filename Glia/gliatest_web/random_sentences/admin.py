b602fb93f51360ebb097e13ad140b8ef7d186b57
AaronLai <aaronlaiya@icloud.com>
2016-01-06 16:45:56 +0800

    Django

---
+++ Glia/gliatest_web/random_sentences/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from random_sentences.models import sentence
+# Register your models here.
+admin.site.register(sentence)
---
+++ Glia/gliatest_web/random_sentences/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random 
+from random_sentences.models import sentence
+
+
+def home(request):
+    S = random.choice(sentence.objects.all())
+    return render(request, "index.html", {'sentence': S})
+
+
+
+
